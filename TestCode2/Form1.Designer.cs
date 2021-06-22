42178227c70ceda6406bad2bae93371456932bcd
jsjw39073907 <junwoo0112@naver.com>
2021-06-22 23:40:20 +0900

    C# 기초2

---
+++ TestCode2/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace TestCode2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPriOut = new System.Windows.Forms.TextBox();
+            this.txtNumOut = new System.Windows.Forms.TextBox();
+            this.txtSale = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPri = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPriOut
+            // 
+            this.txtPriOut.Location = new System.Drawing.Point(104, 287);
+            this.txtPriOut.Name = "txtPriOut";
+            this.txtPriOut.Size = new System.Drawing.Size(250, 21);
+            this.txtPriOut.TabIndex = 33;
+            // 
+            // txtNumOut
+            // 
+            this.txtNumOut.Location = new System.Drawing.Point(104, 229);
+            this.txtNumOut.Name = "txtNumOut";
+            this.txtNumOut.Size = new System.Drawing.Size(250, 21);
+            this.txtNumOut.TabIndex = 31;
+            // 
+            // txtSale
+            // 
+            this.txtSale.Location = new System.Drawing.Point(104, 258);
+            this.txtSale.Name = "txtSale";
+            this.txtSale.Size = new System.Drawing.Size(250, 21);
+            this.txtSale.TabIndex = 32;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "할인액 : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "실구입가 : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "회원번호 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 17F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(39, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 23);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "출력";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 23);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "종료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "할인율 계산";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPri
+            // 
+            this.txtPri.Location = new System.Drawing.Point(104, 91);
+            this.txtPri.Name = "txtPri";
+            this.txtPri.Size = new System.Drawing.Size(250, 21);
+            this.txtPri.TabIndex = 23;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(104, 122);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(250, 21);
+            this.txtAge.TabIndex = 24;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(104, 59);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(250, 21);
+            this.txtNum.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "정가 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "나이 : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "회원번호 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 17F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(39, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "입력";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 337);
+            this.Controls.Add(this.txtPriOut);
+            this.Controls.Add(this.txtNumOut);
+            this.Controls.Add(this.txtSale);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPri);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPriOut;
+        private System.Windows.Forms.TextBox txtNumOut;
+        private System.Windows.Forms.TextBox txtSale;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPri;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestCode2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCode2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sale;
+            txtNumOut.Text = txtNum.Text;
+            if (int.Parse(txtAge.Text) < 18 || int.Parse(txtAge.Text) >= 70)
+                sale = double.Parse(txtPri.Text) * 0.2;
+            else if (int.Parse(txtAge.Text) >= 60 && int.Parse(txtAge.Text) < 70)
+                sale = double.Parse(txtPri.Text) * 0.15;
+            else
+                sale = double.Parse(txtPri.Text) * 0;
+            txtSale.Text = sale.ToString();
+            txtPriOut.Text = (double.Parse(txtPri.Text) - sale).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
