a976a0521c83c62433b5346b62ba8b2afc927d80
jsjw39073907 <junwoo0112@naver.com>
2021-06-26 23:18:49 +0900

    C# 기초6
    이름, 나이, 학번, 주소를 입력받고 복사를 누르면 메시지 박스를 통해 확인하고 각 정보를 출력해준다.

---
+++ TestCode6/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace TestCode6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnCpy = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(30, 193);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(274, 160);
+            this.txtResult.TabIndex = 21;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(191, 153);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(113, 23);
+            this.btnQuit.TabIndex = 20;
+            this.btnQuit.Text = "종료";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.BtnQuit_Click);
+            // 
+            // btnCpy
+            // 
+            this.btnCpy.Location = new System.Drawing.Point(30, 153);
+            this.btnCpy.Name = "btnCpy";
+            this.btnCpy.Size = new System.Drawing.Size(113, 23);
+            this.btnCpy.TabIndex = 19;
+            this.btnCpy.Text = "복사";
+            this.btnCpy.UseVisualStyleBackColor = true;
+            this.btnCpy.Click += new System.EventHandler(this.BtnCpy_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(71, 85);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(115, 21);
+            this.txtNum.TabIndex = 17;
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(71, 112);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(233, 21);
+            this.txtAdd.TabIndex = 18;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(71, 58);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(115, 21);
+            this.txtAge.TabIndex = 16;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(71, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(115, 21);
+            this.txtName.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "주소 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "학번 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "나이 :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "이름 :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(332, 374);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnCpy);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnCpy;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestCode6/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCode6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCpy_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("복사하시겠습니까?", "질문", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                txtResult.Text = "이름 : " + txtName.Text + "\r\n나이 : " + txtAge.Text + "\r\n학번 : " + txtNum.Text + "\r\n주소 : " + txtAdd.Text;
+            }
+        }
+
+        private void BtnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
